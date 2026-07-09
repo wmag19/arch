45d8c5d9828c3fca7df8421836d755db735a6c07
Will Maguire <will_maguire99@protonmail.com>
2026-07-09 11:37:11 +0100

    gb keyboard layout

--- hypr/.config/hypr/config/input.lua
+++ hypr/.config/hypr/config/input.lua
@@ -4,6 +4,7 @@ hl.config({
     input = {
         -- sensitivity = -0.25,
         accel_profile = "flat",
+        kb_layout = "gb"
     },
     -- Uncomment the section below to enable software cursors; this can help with cursor display or behavior issues
     -- cursor = {
@@ -14,4 +15,4 @@ hl.config({
 hl.gesture({ fingers = 4, direction = "horizontal", action = "workspace" })
 hl.gesture({ fingers = 3, direction = "down",       action = "close" })
 hl.gesture({ fingers = 3, direction = "up",         action = "fullscreen" })
-hl.gesture({ fingers = 3, direction = "left",       action = "float" })
+hl.gesture({ fingers = 3, direction = "left", action = "float" })
