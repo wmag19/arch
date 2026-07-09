03c40650b9e1c7be01b9fd10ec880d29d7106122
Will Maguire <will_maguire99@protonmail.com>
2026-07-09 15:16:13 +0100

    Fix monitor refresh rate

--- hypr/.config/hypr/config/monitors.lua
+++ hypr/.config/hypr/config/monitors.lua
@@ -12,5 +12,5 @@
 -- hl.monitor({ output = "DP-2", mode = "1920x1080", position = "1920x0", scale = 1 })
 
 hl.monitor({ output = "eDP-1", mode = "2880x1920@120", position = "0x0", scale = 2 })
-hl.monitor({ output = "DP-10", mode = "3440x1440@143.923", position = "1440x0", scale = 1 })
+hl.monitor({ output = "DP-10", mode = "3440x1440@100", position = "1440x0", scale = 1 })
 hl.monitor({ output = "DP-11", mode = "1920x1080@60", position = "4880x0", scale = 1 })
