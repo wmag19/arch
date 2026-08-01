5d150812ffc884aee492ebe5a47d9e67e964308f
Will Maguire <will_maguire99@protonmail.com>
2026-08-01 13:52:22 +0100

    Change to using cosmic-files for file browsing

--- hypr/.config/hypr/config/defaults.lua
+++ hypr/.config/hypr/config/defaults.lua
@@ -1,7 +1,7 @@
 -- Hyprland default apps
 
 TERMINAL     = "ghostty"
-FILE_MANAGER = "dolphin"
+FILE_MANAGER = "cosmic-files"
 BROWSER      = "firefox"
 EDITOR       = "gnome-text-editor --new-window"
 CALCULATOR   = "gnome-calculator"
